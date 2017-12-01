df6d671643252f9f09b533a4a2c1de2e9d3e79bf
munawirali <munawirali@gmail.com>
2017-12-01 15:43:52 +0700

    add feature redis

--- models/JobSeeker.js
+++ models/JobSeeker.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const JobSeekerRedis = require('../lib/JobSeekerRedis');
+
 mongoose.Promise = global.Promise
 mongoose.connection.openUri(`${process.env.APPDB}_${process.env.NODE_ENV}_db`, (err) => {
   if (err) {
@@ -35,5 +37,34 @@ const jobSeekerSchema = new Schema({
     required: true
   }
 })
+
+jobSeekerSchema.post('save', function(doc){
+  // console.log('im here');
+  JobSeekerRedis.set(doc._id.toString(), doc)
+})
+
+jobSeekerSchema.post('findOneAndRemove', function(doc){
+  // console.log('im here');
+  JobSeekerRedis.del(doc._id.toString())
+})
+
+jobSeekerSchema.post('findOneAndUpdate', function(doc){
+  JobSeekerRedis.del(this._conditions._id.toString())
+  JobSeekerRedis.set(this._conditions._id.toString(), this._update)
+  // console.log('im here update',this._conditions._id,' data ', this._update);
+})
+
+// jobSeekerSchema.pre('findOne', async function(next, done){
+//   let self=this
+//   redis = await JobSeekerRedis.get(self._conditions._id.toString())
+//   if (redis !== null) {
+//     console.log('im redis', redis);
+//     // return redis
+//     done()
+//     // next(redis)
+//   } else {
+//     next()
+//   }
+// })
 
 module.exports = mongoose.model('JobSeeker', jobSeekerSchema)
