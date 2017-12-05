49ae70c0aaad07d0e4c95182b425e5e49db2e2aa
sisinduku <saptanto.sindu@gmail.com>
2017-12-05 16:58:28 +0700

    Add Testing Send Email API

--- controllers/SendEmailCtrl.js
+++ controllers/SendEmailCtrl.js
@@ -1,36 +1,31 @@
 const nodemailer = require('nodemailer')
 
 function SendEmailCtrl(req,res) {
-  console.log('im hereeeee', req.body);
-  nodemailer.createTestAccount((err, account) => {
-    let transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: `${process.env.EMAILUSER}`,
-        pass: `${process.env.EMAILPASS}`
-      }
-    })
-    let mailOptions = {
-      from: `"${process.env.EMAILNAME}" <${process.env.EMAILUSER}>`,
-      to: `${req.body.receiver_email}`,
-      subject : `Invite Test and Interview`,
-      text:
-      `We would like to invite you to participate test and interview at :\n
-      Date : 12-12-2017\n
-      Time : 10:00 am\n
-      Place : Plaza Telkom, Center Jakarta\n
-      Thank for your participate\n
-      Best Regard ${req.body.receiver_name}.`
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: `${process.env.EMAILUSER}`,
+      pass: `${process.env.EMAILPASS}`
     }
-    console.log(mailOptions);
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        res.status(500).send(error)
-      } else {
-        console.log(info);
-        res.status(200).send(info)
-      }
-    })
+  })
+  let mailOptions = {
+    from: `"${process.env.EMAILNAME}" <${process.env.EMAILUSER}>`,
+    to: `${req.body.receiver_email}`,
+    subject : `Invite Test and Interview`,
+    text:
+    `We would like to invite you to participate test and interview at :\n
+    Date : 12-12-2017\n
+    Time : 10:00 am\n
+    Place : Plaza Telkom, Center Jakarta\n
+    Thank for your participate\n
+    Best Regard ${process.env.EMAILUSER}.`
+  }
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      res.status(400).send(error)
+    } else {
+      res.status(200).send(info)
+    }
   })
 }
 
